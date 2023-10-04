dba5a7c4b2bfba277382bb3a5d0ed363776d5407
Ricardo Monroy Palacios <144552733+PythonDataAce@users.noreply.github.com>
2023-10-04 08:25:23 -0500

    update linkedin

---
+++ linkedin.py
@@ -0,0 +1,7 @@
+import webbrowser
+
+# URL de tu perfil de LinkedIn
+linkedin_url = "https://www.linkedin.com/in/ricardo-monroy-9b030b288/"
+
+# Abre la URL en el navegador web predeterminado
+webbrowser.open(linkedin_url)
